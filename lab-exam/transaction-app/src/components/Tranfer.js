00e487849508dfd80ab7ead073cdb16eff81df31
Athika Fatima <athikafatima16@gmail.com>
2023-10-13 19:31:20 -0400

    Lab Exam - Transfer component initiated

---
+++ lab-exam/transaction-app/src/components/Tranfer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Transfer() {
+  const sender = "0x2849234wkrnwkfniuy4234k34n232n4kj23n4";
+  const receiver = "0x284y4234k34n232n4kj23n4234wkrnwkfniu";
+  return (
+    <div>
+      <h1>Transfer</h1>
+      <div>
+        <p>From: {sender}</p>
+        <p>To: {receiver}</p>
+        <form>
+          <label for="amount">Amount: </label>
+          <input type="number" id="amount" placeholder="Enter amount" min="1" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Transfer;
