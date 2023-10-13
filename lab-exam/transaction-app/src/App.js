50fa695cb74a888dc7e8e4eed3309aa1efa36806
Athika Fatima <athikafatima16@gmail.com>
2023-10-13 19:59:40 -0400

    Lab Exam - Transfer component form handled and conditional redenring done for Receipt Component

--- lab-exam/transaction-app/src/App.js
+++ lab-exam/transaction-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Transfer from "./components/Tranfer";
+// import Transfer from "./components/Tranfer";
+import Receipt from "./components/Receipt";
 
 function App() {
   return (
     <div className="App">
-      <Transfer />
+      <Receipt />
     </div>
   );
 }
--- lab-exam/transaction-app/src/components/Tranfer.js
+++ lab-exam/transaction-app/src/components/Tranfer.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Receipt from "./Receipt";
 
 function Transfer() {
   const sender = "0x2849234wkrnwkfniuy4234k34n232n4kj23n4";
   const receiver = "0x284y4234k34n232n4kj23n4234wkrnwkfniu";
 
-  const handleSubmit = () => {};
+  const [amount, setAmount] = useState("");
+  const [showReceipt, setShowReceipt] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowReceipt(true);
+  };
   return (
     <div>
-      <h1>Transfer</h1>
-      <div>
-        <p>From: {sender}</p>
-        <p>To: {receiver}</p>
-        <form>
-          <label for="amount">Amount: </label>
-          <input type="number" id="amount" placeholder="Enter amount" min="1" />
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </form>
+      <div className="transfer-div">
+        <h1>Transfer</h1>
+        <div>
+          <p>From: {sender}</p>
+          <p>To: {receiver}</p>
+          <form>
+            <label for="amount">Amount: </label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              placeholder="Enter amount"
+              min="1"
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
+      {showReceipt && <Receipt />}
     </div>
   );
 }
