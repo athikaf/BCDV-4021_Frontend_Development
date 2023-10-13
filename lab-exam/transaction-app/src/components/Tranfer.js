28e75eacaf7fb65f51814b4b9ccf168499021fd2
Athika Fatima <athikafatima16@gmail.com>
2023-10-13 19:37:46 -0400

    Lab Exam - Transfer component form created

--- lab-exam/transaction-app/src/components/Tranfer.js
+++ lab-exam/transaction-app/src/components/Tranfer.js
@@ -3,6 +3,8 @@ import React from "react";
 function Transfer() {
   const sender = "0x2849234wkrnwkfniuy4234k34n232n4kj23n4";
   const receiver = "0x284y4234k34n232n4kj23n4234wkrnwkfniu";
+
+  const handleSubmit = () => {};
   return (
     <div>
       <h1>Transfer</h1>
@@ -12,6 +14,9 @@ function Transfer() {
         <form>
           <label for="amount">Amount: </label>
           <input type="number" id="amount" placeholder="Enter amount" min="1" />
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
