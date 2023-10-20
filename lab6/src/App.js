266dbcd774df352b6ec0097d7daf49f0824ef195
Athika Fatima <athikafatima16@gmail.com>
2023-10-19 20:57:55 -0400

    Lab6: App completed

--- lab6/src/App.js
+++ lab6/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+}
+function UserProfile({ match }) {
+  const userId = match.params.id;
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>User ID: {userId}</p>
+    </div>
+  );
+}
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/dashboard"
+          element={loggedIn ? <Dashboard /> : <Navigate to="/login" />}
+        ></Route>
+        <Route path="/login" component={Login} />
+        <Route path="/user/:id" component={UserProfile} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </Router>
   );
 }
 
